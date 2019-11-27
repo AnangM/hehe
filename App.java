a91ba112e9ed4ab0818b56ad86625f846f350fb8
AnangM <social.anangmb@gmail.com>
2019-11-27 17:00:12 +0700

    add comment

--- App.java
+++ App.java
@@ -4,11 +4,16 @@ import java.io.IOException;
 
 class App{
     public static void main(String[] args){
+        // membuat user
         User user = new User("Budi","qwerty");
+        // membuat pelajaran
         Pelajaran mtk = new Pelajaran("Matematika",80);
         Pelajaran ipa = new Pelajaran("IPA",70);
+        //memasukkan pelajaran ke user
         user.addPelajaran(mtk);
         user.addPelajaran(ipa);
+
+        //cetak data
         System.out.println("Highest -> " + user.getTertinggi().getNilai());
         System.out.println("Lowest -> " + user.getTerendah().getNilai());
         System.out.println("Average -> " + user.getRataRata());
